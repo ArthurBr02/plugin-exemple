9b1853bd58a9118294f86a783fefdceb2ae0016d
Arthur <34396214+ArthurBr02@users.noreply.github.com>
2025-01-19 22:07:36 +0100

    first commit

---
+++ src/main/java/fr/arthurbr02/pluginExemple/PluginExemple.java
@@ -0,0 +1,17 @@
+package fr.arthurbr02.pluginExemple;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PluginExemple extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
